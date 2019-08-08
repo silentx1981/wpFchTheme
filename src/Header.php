6aa07e2da8ec3ec6c2ecc483db1635af1ea54a58
Raffael Wyss <raffael.wyss@gmail.com>
2019-08-08 16:37:33 +0200

    Header Optimiert

--- src/Header.php
+++ src/Header.php
@@ -8,7 +8,7 @@ class Header
 	public function show()
 	{
 		$headerImages = [];
-		for ($i = 0; $i < 5; $i++) {
+		for ($i = 0; $i <= 5; $i++) {
 			$nr = $i == 0 ? '' : $i;
 			$image = get_theme_mod("image_header_background$nr");
 			if ($image)
@@ -19,7 +19,7 @@ class Header
 		}
 		$random = rand(0, count($headerImages) - 1);
 		$headerImages[$random]['active'] = 'active';
-		$headerImages = [$headerImages[$random]];
+		$headerImages = [$headerImages[$random] ?? $headerImages[0]];
 		include_once('template/header.tpl.php');
 	}
 
