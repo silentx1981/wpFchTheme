24e211a428d999ef72080512a7cbd1c0a1837b76
Raffael Wyss <raffael.wyss@gmail.com>
2020-02-02 18:16:48 +0100

    Add Spielbetrieb-Slider1

--- src/template/spielbetrieb.slider1.php
+++ src/template/spielbetrieb.slider1.php
@@ -1,18 +1,4 @@
-
-<style>
-    .rowSlider {
-        display: -webkit-box;
-        display: -webkit-flex;
-        display: -ms-flexbox;
-        display:         flex;
-    }
-    .rowSlider > [class*='col-'] {
-        display: flex;
-        flex-direction: column;
-    }
-</style>
-
-<div class="container text-center">
+<div class="container">
     <div class="row mx-auto my-auto">
         <div id="spielBetriebCarousel" class="carousel slide w-100" data-ride="carousel" data-interval="false">
             <div class="carousel-inner" role="listbox"><?php
@@ -24,7 +10,7 @@
                             ?>
                             <div class="col-sm-<?php echo floor(12 / $grid); ?>" style="margin-bottom: 20px;">
                                 <div class="card h-100">
-                                    <div class="card-header">
+                                    <div class="card-header text-center">
                                         <strong>
                                             <?php echo $locale['wochentagLong'][date('N', strtotime($spieldatum))].' '.date("d.m.Y", strtotime($spieldatum)); ?>
                                         </strong>
@@ -33,26 +19,43 @@
                                         <?php
                                         foreach ($spiele['spiele'] as $spiel) {
                                             ?>
-                                            <div class="container-fluid">
+                                            <div class="container">
                                                 <div class="row">
                                                     <div class="col-12">
-                                                        <span class="badge <?php echo $spiel['TypClass']; ?> w-100" style="overflow: hidden">
+                                                        <span class="badge <?php echo $spiel['TypClass']; ?> w-100" style="overflow: hidden;  margin-bottom: 10px;">
                                                             <?php echo $spiel['Typ'] ?>
                                                         </span>
                                                     </div>
-                                                    <div class="col-12">
-                                                        <?php echo date('H:i', strtotime($spiel['Datumzeit'])); ?>
+                                                    <?php
+                                                    if (isset($spiel['Status']) && $spiel['Status'] !== '') { ?>
+                                                        <div class="col-12">
+                                                            <div class="d-flex mb-2 text-danger">
+                                                                <div class="align-self-start text-center" style="width: 50px;"> <i class="fas fa-exclamation-triangle"></i>&nbsp;</div>
+                                                                <div class="align-self-center"><?php echo $spiel['Status']; ?></div>
+                                                            </div>
+                                                        </div>
                                                         <?php
-                                                        if ($spiel['Location'] !== '') {
-                                                            ?>
-                                                            <span data-toggle="tooltip" data-placement="top" title="<?php echo $spiel['Location']; ?>">
-                                                                <i class="fas fa-map-marker-alt"></i>
-                                                            </span>
-                                                            <?php
-                                                        }
-                                                        ?>
+                                                    }
+                                                    ?>
+                                                    <div class="col-12">
+                                                        <div class="d-flex mb-2">
+                                                            <div class="align-self-start text-center" style="width: 50px;"> <i class="far fa-2x fa-clock"></i>&nbsp;</div>
+                                                            <div class="align-self-center"><?php echo date('H:i', strtotime($spiel['Datumzeit'])); ?></div>
+                                                        </div>
                                                     </div>
-                                                    <div class="col-10">
+                                                    <?php
+                                                    if (!empty($spiel['Location'])) {
+                                                        ?>
+                                                        <div class="col-12">
+                                                            <div class="d-flex mb-2">
+                                                                <div class="align-self-start text-center" style="min-width: 50px;"><i class="fas fa-2x fa-map-marker-alt"></i>&nbsp;</div>
+                                                                <div class="align-self-center"><?php echo $spiel['Location']; ?></div>
+                                                            </div>
+                                                        </div>
+                                                        <?php
+                                                    }
+                                                    ?>
+                                                    <div class="col-10" style="margin-top: 10px;">
                                                         <div class="row">
                                                             <div class="col-12 <?php if ($spiel['HomeTeam'] === 'TeamA') echo 'font-weight-bold'; ?>">
                                                                 <?php echo $spiel['TeamA']; ?>
@@ -62,22 +65,14 @@
                                                             </div>
                                                         </div>
                                                     </div>
-                                                    <div class="col-2">
+                                                    <div class="col-2" style="margin-top: 10px;">
                                                         <div class="row">
-                                                            <div class="col-12">
+                                                            <div class="col-12 <?php if ($spiel['HomeTeam'] === 'TeamA') echo 'font-weight-bold'; ?>">
                                                                 <span class="text-right"><?php echo($spiel['TorA'] ?? ''); ?></span>
                                                             </div>
-                                                            <div class="col-12"><?php echo($spiel['TorB'] ??
-                                                                    ''); ?></div>
+                                                            <div class="col-12 <?php if ($spiel['HomeTeam'] === 'TeamB') echo 'font-weight-bold'; ?>"><?php echo($spiel['TorB'] ?? ''); ?></div>
                                                         </div>
                                                     </div>
-                                                    <?php if (isset($spiel['Status']) && $spiel['Status'] !== '') { ?>
-                                                        <div class="col-12 text-danger">
-                                                            <small>
-                                                                <i class="fas fa-exclamation-triangle"></i> <?php echo $spiel['Status']; ?>
-                                                            </small>
-                                                        </div>
-                                                    <?php } ?>
                                                 </div>
                                             </div>
                                             <hr>
